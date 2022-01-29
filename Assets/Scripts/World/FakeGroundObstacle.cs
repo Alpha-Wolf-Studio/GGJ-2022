e8c037a1f3a1c2dd3e392d0787696ea4bde9db6e
Adrian <Owinmowe@gmail.com>
2022-01-29 16:44:46 -0300

    Added fake ground obstacle.

---
+++ Assets/Scripts/World/FakeGroundObstacle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FakeGroundObstacle : MonoBehaviour, IObstacle
+{
+    [SerializeField] SpriteRenderer baseRenderer;
+    [SerializeField] SpriteRenderer bgRenderer;
+
+    public void Activate()
+    {
+        baseRenderer.color = Utils.OniWhite;
+    }
+
+    public void Disactivate()
+    {
+        baseRenderer.color = Utils.OniWhite;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        baseRenderer.enabled = false;
+        bgRenderer.enabled = false;
+    }
+}
