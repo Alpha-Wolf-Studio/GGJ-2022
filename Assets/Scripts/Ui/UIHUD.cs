2d2fcb54397e2cb7a004b3a4c8d3b4e1c129f218
Mauro <mauro.mendozacp@gmail.com>
2022-01-29 20:39:22 -0300

    ui hud implemented

---
+++ Assets/Scripts/Ui/UIHUD.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UIHUD : MonoBehaviour
+{
+    [SerializeField] private TMP_Text pearlsText = null;
+
+    private void Start()
+    {
+        GameManager.Get().OnCurrencyChange += PearlsUpdate;
+    }
+
+    private void PearlsUpdate(int pearlsValue)
+    {
+        pearlsText.text = pearlsValue.ToString();
+    }
+}
