6fda61c831abab36432add7b21a86f6777d2b626
Federico Olive <FedericoOlive@gmail.com>
2022-01-30 02:12:53 -0300

    Add levels.

--- Assets/Scripts/World/Spike.cs
+++ Assets/Scripts/World/Spike.cs
@@ -1,27 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
 using UnityEngine;
-
 public class Spike : MonoBehaviour, IObstacle
 {
     public Action OnTouch;
-
-    SpriteRenderer rend;
-    private void Start()
-    {
-        rend = GetComponent<SpriteRenderer>();
-    }
-
+    
     public void Activate()
     {
-        rend.color = Utils.OniWhite;
     }
-
     public void Disactivate()
     {
-        rend.color = Utils.OniBlack;
     }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         OnTouch?.Invoke();
